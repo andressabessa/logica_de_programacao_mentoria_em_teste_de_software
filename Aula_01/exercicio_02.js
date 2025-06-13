9c4742ae974229db03807d1cdb592314eb7a9390
andressa bessa <andressa.bessa@atlastechnol.com>
2025-06-13 09:08:03 -0300

    feat: add exercise 02

---
+++ Aula_01/exercicio_02.js
@@ -0,0 +1,11 @@
+// Informe o valor do produto que você quer comprar
+const valorDoProduto = 120.00
+
+// Informe quanto você tem disponível
+const valorDisponivel = 80.00
+
+// Subtraia o valor disponível do valor do produto
+const valorFaltante = valorDoProduto - valorDisponivel
+
+// Escreva o resultado da sua conta
+console.log(valorFaltante)
