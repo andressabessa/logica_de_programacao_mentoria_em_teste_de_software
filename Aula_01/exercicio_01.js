7fca698d9a865387b76f07ee84d1194f0793b837
andressa bessa <andressa.bessa@atlastechnol.com>
2025-06-13 09:05:43 -0300

    feat: add exercise 01

---
+++ Aula_01/exercicio_01.js
@@ -0,0 +1,11 @@
+//  Informe qual o valor do produto
+const valorDoProduto = 98.00
+
+// Informe qual o valor do desconto 
+const valorDoDesconto = 15.00
+
+// Subtraia o desconto do valor original 
+const resultadoFinal = valorDoProduto - valorDoDesconto
+
+// Escreva o resultado da sua conta
+console.log(resultadoFinal)
