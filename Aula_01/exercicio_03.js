2fee409eac622ca6ff6a3fc02bb78f448070e6a0
andressa bessa <andressa.bessa@atlastechnol.com>
2025-06-13 09:08:15 -0300

    feat: add exercise 03

---
+++ Aula_01/exercicio_03.js
@@ -0,0 +1,11 @@
+// Informe o valor original do produto
+const valorOriginal = 30.00
+
+// Informe o valor do aumento
+const aumento = 5.00
+
+// Some o valor original com o aumento
+const valorFinal = valorOriginal + aumento
+
+// Escreva o resultado da sua conta
+console.log(valorFinal)
