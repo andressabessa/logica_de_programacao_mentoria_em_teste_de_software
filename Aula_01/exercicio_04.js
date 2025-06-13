4cd852fb1ca1319704cd39676c0a954cc5fa1d63
andressa bessa <andressa.bessa@atlastechnol.com>
2025-06-13 09:08:30 -0300

    feat: add exercise 04

---
+++ Aula_01/exercicio_04.js
@@ -0,0 +1,23 @@
+// Informe o valor unitário da blusa
+const precoBlusa = 46.00
+
+// Informe a quantidade de blusas que deseja comprar
+const quantidadeBlusas = 2
+
+// Calcule o total gasto com blusas
+const totalBlusas = precoBlusa * quantidadeBlusas
+
+// Informe o valor unitário do vestido
+const precoVestido = 75.00
+
+// Informe a quantidade de vestido que deseja comprar
+const quantidadeVestidos = 3
+
+// Calcule o total gasto com vestidos
+const totalVestidos = precoVestido * quantidadeVestidos
+
+// Some os totais para saber quanto vai pagar no total
+const totalGeral = totalBlusas + totalVestidos
+
+// Escreva o resultado da sua conta
+console.log(totalGeral)
